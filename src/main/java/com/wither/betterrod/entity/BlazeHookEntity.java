33183bf1f2a8434fd52b76cc544b1de0a005f641
Wither <75589296@qq.com>
2026-06-24 11:46:03 +0800

    烈焰钩可烧炼物品

--- src/main/java/com/wither/betterrod/entity/BlazeHookEntity.java
+++ src/main/java/com/wither/betterrod/entity/BlazeHookEntity.java
@@ -2,13 +2,22 @@ package com.wither.betterrod.entity;
 
 import com.wither.betterrod.init.EntityRegister;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.Blaze;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
+import net.minecraft.world.item.crafting.SmeltingRecipe;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 public class BlazeHookEntity extends FireproofHookEntity{
     public BlazeHookEntity(Player player, Level level, int luck, int lureSpeed) {super(player, level, luck, lureSpeed);}
@@ -40,6 +49,25 @@ public class BlazeHookEntity extends FireproofHookEntity{
             if(level().getBlockState(pos).is(Blocks.ICE)) {
                 level().destroyBlock(pos, true, this.getOwner());
                 level().setBlock(pos, Blocks.WATER.defaultBlockState(), 3);
+            }
+        }
+        Entity hookedIn = this.getHookedIn();
+        if(hookedIn != null && !hookedIn.isOnFire() && ! hookedIn.isInWaterOrRain()) {
+            hookedIn.setRemainingFireTicks(20);
+            if(hookedIn instanceof ItemEntity item && this.level() instanceof ServerLevel serverLevel){
+                ItemStack itemStack = item.getItem();
+                SingleRecipeInput input = new SingleRecipeInput(itemStack);
+                Optional<RecipeHolder<@NotNull SmeltingRecipe>> optional = serverLevel.recipeAccess().getRecipeFor(
+                        RecipeType.SMELTING,
+                        input,
+                        serverLevel
+                );
+                if(optional.isPresent()) {
+                    SmeltingRecipe recipe = optional.get().value();
+                    ItemStack newItem = recipe.assemble(input);
+                    newItem.setCount(itemStack.getCount());
+                    item.setItem(newItem);
+                }
             }
         }
     }
